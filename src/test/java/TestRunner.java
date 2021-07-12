08602f977ba9fc124ff010a712b9b117347fbe9d
JuliaMelnichenkoYTree <juliam@y-tree.com>
2021-07-12 11:34:57 +0300

    fixed

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,7 +1,15 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(glue = {"steps"}, features = {"src/test/resources/features"})
+@CucumberOptions(
+        glue = {"steps"},
+        features = {"src/test/resources/features"},
+        plugin = {
+                "pretty",
+                "summary",
+                "html:build/cucumber-reports/cucumber-pretty",
+                "json:build/cucumber-reports/CucumberTestReport.json",
+                "rerun:build/cucumber-reports/rerun.txt"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
